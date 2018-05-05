88238a21d0b95fc95e1c5b7b350620caa3a48520
junwooji <junwoo@finda.co.kr>
2018-05-06 00:59:51 +0900

    - smartcontract is working well in testnet

--- src/VotingManager.js
+++ src/VotingManager.js
@@ -7,12 +7,11 @@ var VotingItem = function(item) {
         this.id = obj.id;
         this.title = obj.title;
         this.choice = obj.choice;
-        this. author = obj.author;
+        this.author = obj.author;
     } else {
-        this.id = 0
-        this.title = ''
+        this.title = '';
         this.choice = [];
-        this. author = '';
+        this.author = '';
     }
 };
 
@@ -34,32 +33,32 @@ var VotingManager = function () {
 };
 
 VotingManager.prototype = {
-    init: function() { 
-        this.votingItemRepo.put('MAXID', 0);
-    },
+    init: function() {},
 
     enroll: function(title, choice) {
         if (title === '' || choice.length === 0) throw new Error("empty title or choice");
         if (title.length > 64) throw new Error("title exceed limit length");
 
+        var id = this.votingItemRepo.get('MAXID') ? this.votingItemRepo.get('MAXID') + 1 : 1;
+        var author = Blockchain.transaction.from;
+
         var votingItem = this.votingItemRepo.get(id);
         if (votingItem) throw new Error("Voting Item has been occupied");
 
-        var author = Blockchain.transaction.from;
-        // TODO: id setting
         votingItem = new VotingItem();
-        votingItem.id = this.votingItemRepo.get('MAXID');
+        votingItem.id = id;
         votingItem.author = author;
         votingItem.title = title;
         votingItem.choice = choice;
         this.votingItemRepo.put('MAXID', votingItem.id);
         this.votingItemRepo.put(id, votingItem);
+        return votingItem;
     },
 
     get: function(id) {
         if (id === undefined) throw new Error("id is invalid")
         var votingItem = this.votingItemRepo.get(id);
-        if (votingItem !== undefined) throw new Error("votingItem is not existed")
+        if (votingItem === undefined) throw new Error("votingItem is not existed")
         return votingItem;
     },
 };
