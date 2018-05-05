9cd7b80aaa48b35889af71ef8415910a732670dc
junwooji <junwoo@finda.co.kr>
2018-05-06 00:15:54 +0900

    - rename Smart Contract file -> VotingManager.js, - add get function - save max id in votingItemRepo 'MAXID'

--- src/VotingManager.js
+++ src/VotingManager.js
@@ -14,13 +14,13 @@ var VotingItem = function(item) {
         this.choice = [];
         this. author = '';
     }
-}
+};
 
 VotingItem.prototype = {
     toString: function() {
         return JSON.stringify(this);
     }
-}
+};
 
 var VotingManager = function () {
     LocalContractStorage.defineMapProperty(this, "votingItemRepo", {
@@ -29,14 +29,17 @@ var VotingManager = function () {
         },
         stringify: function (o) {
             return o.toString();
-        }
+        },
     });
 };
 
 VotingManager.prototype = {
-    init: function() { },
+    init: function() { 
+        this.votingItemRepo.put('MAXID', 0);
+    },
+
     enroll: function(title, choice) {
-        if(title === '' || choice.length === 0) throw new Error("empty title / choice");
+        if (title === '' || choice.length === 0) throw new Error("empty title or choice");
         if (title.length > 64) throw new Error("title exceed limit length");
 
         var votingItem = this.votingItemRepo.get(id);
@@ -45,11 +48,20 @@ VotingManager.prototype = {
         var author = Blockchain.transaction.from;
         // TODO: id setting
         votingItem = new VotingItem();
+        votingItem.id = this.votingItemRepo.get('MAXID');
         votingItem.author = author;
         votingItem.title = title;
         votingItem.choice = choice;
+        this.votingItemRepo.put('MAXID', votingItem.id);
         this.votingItemRepo.put(id, votingItem);
-    }
-}
+    },
+
+    get: function(id) {
+        if (id === undefined) throw new Error("id is invalid")
+        var votingItem = this.votingItemRepo.get(id);
+        if (votingItem !== undefined) throw new Error("votingItem is not existed")
+        return votingItem;
+    },
+};
 
 module.exports = VotingManager;
