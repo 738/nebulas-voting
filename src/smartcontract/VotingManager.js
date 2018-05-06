65eb2d7238c8df007c6adb9794f16e44bc8b33ec
junwooji <junwoo@finda.co.kr>
2018-05-06 21:06:02 +0900

    add author parameter in enroll func, prevent duplicate voting

--- src/smartcontract/VotingManager.js
+++ src/smartcontract/VotingManager.js
@@ -35,24 +35,23 @@ const maxId = 'MAXID';
 VotingManager.prototype = {
     init: function() {},
 
-    enroll: function(title, choices) {
+    enroll: function(title, choices, author) {
         var choicesParsed = choices.split('|');
         if (title === '' || choices.length === 0) throw new Error("empty title or choices");
         if (title.length > 64) throw new Error("title exceed limit length");
 
         var id = LocalContractStorage.get(maxId) ? LocalContractStorage.get(maxId) + 1 : 1;
-        var author = Blockchain.transaction.from;
 
         var votingItem = this.votingItemRepo.get(id);
         if (votingItem) throw new Error("Voting Item has been occupied");
 
         votingItem = new VotingItem();
         votingItem.id = id;
-        votingItem.author = author;
+        votingItem.author = author || Blockchain.transaction.from;
         votingItem.title = title;
-        // [['AAA', 0], ['BBB', 0], ['CCC', 0]] 형식으로 저장
+        // [['AAA', []], ['BBB', []], ['CCC', []]] 형식으로 저장
         var choicesWithNumber = [];
-        for(var choice of choicesParsed) choicesWithNumber.push([choice, 0]);
+        for(var choice of choicesParsed) choicesWithNumber.push([choice, []]);
         votingItem.choices = choicesWithNumber;
         LocalContractStorage.put(maxId, votingItem.id);
         this.votingItemRepo.put(id, votingItem);
@@ -78,8 +77,15 @@ VotingManager.prototype = {
 
     vote: function(id, index) {
         var votingItem = this.get(id);
-        if (votingItem.choices.length <= index) throw new Error("There is no voting choice in that index")
-        votingItem.choices[index][1] += 1;
+        if (votingItem.choices.length <= index) throw new Error("There is no voting choice in that index");
+        var voterAddress = Blockchain.transaction.from;
+        // 한 사람이 같은 투표에 여러 번 투표할 수 없음
+        for(var i = 0; i < votingItem.choices.length; i++) {
+            for(var j = 0; j < votingItem.choices[i][1].length; j++) {
+                if (voterAddress === votingItem.choices[i][1][j]) return 'you can vote just once';
+            }
+        }
+        votingItem.choices[index][1].push(Blockchain.transaction.from);
         this.votingItemRepo.put(id, votingItem);
         return votingItem;
     }
