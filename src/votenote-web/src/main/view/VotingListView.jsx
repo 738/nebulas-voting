c5211684250300f7515895c3b6c866778b6f9ecf
junwooji <junwoo@finda.co.kr>
2018-05-07 15:06:56 +0900

    add style of voting list

--- src/votenote-web/src/main/view/VotingListView.jsx
+++ src/votenote-web/src/main/view/VotingListView.jsx
@@ -9,10 +9,13 @@ class VotingListView extends Component {
 
     render() {
         return (
-            <div className="votingListViewContainer">
+            <div className="VotingListView-Container">
+                <h1 className="VotingListView-title">Voting List</h1>
+                <VotingItemView votingItem={{id: 'id', title: 'title', author: 'author'}}></VotingItemView>
                 {this.props.votingItems.map((item, index) => 
                     <VotingItemView key={index} votingItem={item}></VotingItemView>
                 )}
+                <div className="VotingListView-enroll-button">enroll vote</div>
             </div>
         );
     }
