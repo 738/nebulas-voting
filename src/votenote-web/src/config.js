c377b851e308ae939ddc9dce87cf5c0e070bcbe9
junwooji <junwoo@finda.co.kr>
2018-05-13 17:37:28 +0900

    update Mainnet smartcontract address

--- src/votenote-web/src/config.js
+++ src/votenote-web/src/config.js
@@ -1,3 +1,5 @@
-const dappAddress = "n1wES4kVUoaPjA3PfjiM8h17E19qcqkWCiA";
+const dappAddress = "n1ovFARWjeguJipEZ4j4SYq3RSFZwy9io3z";
+// mainnet: n1ovFARWjeguJipEZ4j4SYq3RSFZwy9io3z
+// testnet: n1wES4kVUoaPjA3PfjiM8h17E19qcqkWCiA
 
 export { dappAddress };
