91464858b40235ebfa21d0869515ba0ab9c8d570
지준우 <junwoo@finda.co.kr>
2018-06-04 22:38:14 +0900

    webpack setting

---
+++ src/votenote-web/webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 7777,
+        contentBase: __dirname + '/public/'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    }
+};
